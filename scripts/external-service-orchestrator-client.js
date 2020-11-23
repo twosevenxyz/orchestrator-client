d9caa28e663263f28705dd416b6a5640ec53efb6
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2020-11-22 23:13:10 -0500

    Added script to perform heartbeats for external, third-party services

---
+++ scripts/external-service-orchestrator-client.js
@@ -0,0 +1,28 @@
+const OrchestratorClient = require('../index')
+const findProcess = require('find-process')
+const argv = require('minimist')(process.argv.slice(2))
+
+const url = argv['orchestrator-url'] || process.env.ORCHESTRATOR_URL
+const secret = argv['orchestrator-secret'] || process.env.ORCHESTRATOR_SECRET
+const externalServiceName = argv['external-service-name'] || process.env.ORCHESTRATOR_EXTERNAL_SERVICE_NAME
+
+const port = argv['orchestrator-service-port'] || process.env.ORCHESTRATOR_SERVICE_PORT
+
+;(async () => {
+  const client = new OrchestratorClient(url, secret)
+  await client.init(port)
+  // At this point, the heartbeat task should've been set up. Hijack it
+  const { heartbeatTask } = client
+  let { heartbeat: origHeartbeatFn } = heartbeatTask
+  origHeartbeatFn = origHeartbeatFn.bind(heartbeatTask)
+
+  const minProcessCount = argv['external-service-name'] ? 2 : 1 // If the process name is specified in argv, then this node process always be reported by find-process. So we expect 2 instead
+  heartbeatTask.heartbeat = async () => {
+    const processes = await findProcess('name', externalServiceName)
+    if (processes.length < minProcessCount) { // The process itself will always be
+      // Do not trigger heartbeat
+      return
+    }
+    origHeartbeatFn()
+  }
+})()
