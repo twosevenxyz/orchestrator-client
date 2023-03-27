a41ea58631e12cf962a2d85a453ca05301f0b628
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2023-03-27 15:32:19 -0400

    Add signal handling

--- scripts/external-service-orchestrator-client.js
+++ scripts/external-service-orchestrator-client.js
@@ -28,4 +28,24 @@ const port = argv['orchestrator-service-port'] || process.env.ORCHESTRATOR_SERVI
       origHeartbeatFn()
     }
   }
+
+  const signals = {
+    SIGHUP: 1,
+    SIGINT: 2,
+    SIGTERM: 15
+  }
+  // Do any necessary shutdown logic for our application here
+  const shutdown = async (signal, value) => {
+    console.log('shutdown!')
+    await client.destroy()
+    console.log(`external-service-orchestrator-client stopped by ${signal} with value ${value}`)
+    process.exit(128 + value)
+  }
+  // Create a listener for each of the signals that we want to handle
+  Object.keys(signals).forEach((signal) => {
+    process.on(signal, () => {
+      console.log(`process received a ${signal} signal`)
+      shutdown(signal, signals[signal])
+    })
+  })
 })()
